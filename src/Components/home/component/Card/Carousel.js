92a95031d3aeebd49dd53efc28adcd1eb44aa3a0
suppakorn171 <47360904+suppakorn171@users.noreply.github.com>
2022-04-27 15:36:14 +0700

    -

--- src/Components/home/component/Card/Carousel.js
+++ src/Components/home/component/Card/Carousel.js
@@ -11,43 +11,7 @@ import './Cards.css'
 const Carousel = () => {
   return (
     <MDBContainer>
-      <MDBCarousel
-        activeItem={1}
-        length={3}
-        showControls={true}
-        showIndicators={true}
-        className="z-depth-1"
-      >
-        <MDBCarouselInner>
-          <MDBCarouselItem itemId="1">
-            <MDBView>
-              <img
-                className="d-block w-100 fixP1"
-                src={pic1}
-                alt="First slide"
-              />
-            </MDBView>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId="2">
-            <MDBView>
-              <img
-                className="d-block w-100 fixP2"
-                src={pic2}
-                alt="Second slide"
-              />
-            </MDBView>
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId="3">
-            <MDBView>
-              <img
-                className="d-block w-100 fixP3"
-                src={pic3}
-                alt="Third slide"
-              />
-            </MDBView>
-          </MDBCarouselItem>
-        </MDBCarouselInner>
-      </MDBCarousel>
+
     </MDBContainer>
   );
 }
